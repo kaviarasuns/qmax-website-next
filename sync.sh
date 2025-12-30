ca4239cf62bbcd0f98a8dc21b7275e2c602f3536
Kaviarasu S <kaviarasus@Kaviarasus-Mac-mini.local>
2025-12-30 18:29:54 +0530

    feat: Add S3 sync for qmaxsys.com and specify CloudFront distribution ID for invalidation.

--- sync.sh
+++ sync.sh
@@ -1,4 +1,7 @@
 aws s3 rm s3://qmax-website-v2 --recursive
 aws s3 sync ./out s3://qmax-website-v2
 
-aws cloudfront create-invalidation --distribution-id YOUR_DISTRIBUTION_ID --paths "/*"
+aws s3 rm s3://qmaxsys.com --recursive
+aws s3 sync ./out s3://qmaxsys.com
+
+aws cloudfront create-invalidation --distribution-id DISTRIBUTION_ID --paths "/*"
