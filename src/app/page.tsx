80535e33f43605ed9831621d34e1b084d8420df1
Kaviarasu S <kaviarasus@Kaviarasus-Mac-mini.local>
2026-04-27 22:49:10 +0530

    eslint fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import ScrollCardsAnimationV4 from "@/components/concept-to-manufacturing-v4";
 import { ServicesSection } from "@/components/services-section";
 import InsideOutV2 from "@/components/InsideOut-V2";
-import ServicesV4Stacked from "@/components/Services-V4-Stacked";
 import { IndustriesWeServeV2 } from "@/components/industries-we-serve-v2";
 import HeroSection from "@/components/home/hero-section";
 import FullPageScroll from "@/components/home/full-page-scroll";
