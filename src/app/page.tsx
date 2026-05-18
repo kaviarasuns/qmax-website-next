cfaabceb0c07f32cbccd80ea7ff2ca5e77fdcaf7
Kaviarasu S <kaviarasus@qmaxsys.com>
2026-05-18 09:42:25 +0530

    replace Our Services section in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import HeroSection from "@/components/home/hero-section";
 import FooterV2 from "@/components/footer-v2";
 import { IndustriesWeServeV3 } from "@/components/industries-we-serve-v3";
 import StackReveal from "@/components/StackReveal";
-import ServicesV2 from "@/components/Services-V2";
+import OurServices from "@/components/our-services";
 
 export default function Home() {
   return (
@@ -15,8 +15,8 @@ export default function Home() {
       <StackReveal />
       {/* <ScrollCardsAnimationV4 onAutoHighlightChange={() => {}} /> */}
       {/* <ServicesV4Stacked lockStackingOnProgrammaticScroll={false} /> */}
-      <ServicesV2 />
-      {/* <OurServices /> */}
+      {/* <ServicesV2 /> */}
+      <OurServices />
       <IndustriesWeServeV3 />
       <InsideOutV2 />
 
